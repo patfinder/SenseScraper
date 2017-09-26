6e38ac0f6e19b566eb833e71d19a1c653fe271e5
Vuong Nguyen <vuong.se@gmail.com>
2017-09-27 02:38:27 +0700

    Follow next page;

--- SenseScraper/spiders/ForumScrawler.py
+++ SenseScraper/spiders/ForumScrawler.py
@@ -1,18 +1,35 @@
 # -*- coding: utf-8 -*-
 from uu import encode
 
+import urlparse
 import scrapy
 
 class ForumcrawlerSpider(scrapy.Spider):
     name = 'ForumCrawler'
     allowed_domains = ['tinhte.vn']
-    start_urls = ['https://tinhte.vn/threads/2730624/']
+    base_url = 'https://tinhte.vn/'
+    # start_urls = ['https://tinhte.vn/threads/2730624/']
+    # start_urls = ['https://tinhte.vn/threads/2730624/page-2']
+    start_urls = ['https://tinhte.vn/threads/2731972/']
+
+    runCount = 0
 
     def parse(self, response):
+
+        ForumcrawlerSpider.runCount += 1
+
         selectors = response.xpath('//*[contains(concat(" ", @class, " "), " baseHtml ")]')
-        posts = [" ".join(s.select('./text()').extract()) for s in selectors[1:]]
+        # Skip first item which is the post
+        posts = [" ".join(s.xpath('./text()').extract()) for s in selectors[1:]]
         for p in posts:
-            yield {"PostZ": p.encode("utf8")}
+            yield {"PostZ": p.encode("utf8").strip()}
+
+        # if ForumcrawlerSpider.runCount >= 5:
+        #     return
+
+        # Find next page URL
+        linkSels = response.xpath('//*[contains(concat( " ", @class, " " ), " PageNav ")]//a[text() = "Sau >"]/@href')
+        yield scrapy.Request(urlparse.urljoin(ForumcrawlerSpider.base_url, linkSels.extract_first())) if linkSels else None
 
     @staticmethod
     def selectClass(response, class1):
